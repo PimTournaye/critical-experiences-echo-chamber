3574b2d94c5e835bc28bb6fde2a586b38ea55beb
PimTournaye <pimtournaye1@gmail.com>
2023-10-16 09:50:49 +0200

    update

--- server.ts
+++ server.ts
@@ -10,18 +10,15 @@ const io = new Server(httpServer, {
   /* options */
 });
 
-// check later if we end up making a database
-// so that we can de-associate data by saving responses in a rephrased way
-// https://dictionaryapi.com/products/api-collegiate-thesaurus
-
-
-const countdownMaxTimer = 40;
+const submitStageTimer = 30;
+const cooldownStageTimer = 30;
 const closingMessage =
   "While our views feel certain from within, they are limited without the insights others provide. Broadening our circles leads to growth.";
 
 // Keeping track of things
 let gameState = {
   started: false,
+  currentString: '',
   currentPrompt: null,
   currentSet: null,
   amountOfPromptsDone: 0,
@@ -51,7 +48,7 @@ io.on("connection", (socket) => {
   checkAndStartLoop();
 
   // When we receive a new response to a prompt
-  socket.on("prompt-response", (data) => {
+  socket.on("response-submit", (data) => {
     if (responses.length <= 0) responses.push(data);
     else responses = [...responses, data];
   });
@@ -69,10 +66,12 @@ io.on("connection", (socket) => {
 
 // Function to check conditions and start the loop
 function checkAndStartLoop() {
-  if (locations.length >= 5 && !inPostgameCooldown) {
+  if (locations.length >= 5 && !inPostgameCooldown && !gameState.started) {
     startGame();
   }
 }
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 
 // Countdown/game logic
@@ -86,88 +85,65 @@ function checkAndStartLoop() {
 const startGame = () => {
   // The game has started, so let's set the gamestate to true
   gameState.started = true;
-  // Set the correct set and prompt
-  getPromptSet();
+  // Get the correct set and prompt
+  const currentSet = getPromptSet();
+  const currentPrompt =
   getCurrentPrompt(gameState.currentPrompt);
   // Publish the locations of all the users
   io.emit("locations", locations);
-  // Publish the first prompt
-  io.emit("prompt", gameState.currentPrompt);
-  // After 30 seconds, start the countdown timer
-  setTimeout(() => {
-    // Publish the countdown timer
-    io.emit("countdown", countdownMaxTimer);
-    // Start the countdown timer
-    startCountdown();
-  }, 30000);
+  // Start a setLoop to go through the set of prompts one by one, moving on to the next one after setLoop resolves
 }
 
-const startCountdown = () => {
-  // If the timer has reached 0, start the cooldown timer
-  if (gameState.currentTimer <= 0) startCooldown();
-  // If the timer hasn't reached 0, publish the timer and reduce it by 1
-  else {
-    io.emit("countdown", gameState.currentTimer);
-    gameState.currentTimer--;
-    // Wait 1 second and run the function again
-    setTimeout(() => {
-      startCountdown();
-    }, 1000);
+const setLoop = () => {
+  // Start a prompt loop which resolves after we've gone through the three stringS in the current prompt
+  promptStage(currentPrompt);
+
+  // Start the submit stage
+
+  // Start the cooldown loop
+
+}
+
+// Update the current string every 10 seconds
+const promptStage = async (prompt) => {
+  // For the three strings we have in the prompt, publish them to the users with a 10 second delay in between
+  for (let i = 0; i < prompt.length; i++) {
+    gameState.currentString = prompt[i];
+    io.emit("update-prompt", gameState.currentString);
+    await sleep(10000);
   }
 }
 
-const startCooldown = () => {
-  // If the timer has reached 0, start the cooldown timer
-  if (gameState.currentTimer <= 0) startNextPrompt();
-  // If the timer hasn't reached 0, publish the timer and reduce it by 1
-  else {
-    io.emit("cooldown", gameState.currentTimer);
-    gameState.currentTimer--;
-    // Wait 1 second and run the function again
-    setTimeout(() => {
-      startCooldown();
-    }, 1000);
-  }
+// A window of 30 seconds where users can submit their responses
+const submitStage = () => {
 }
 
-const getPromptSet = () => {
+// A timer that lasts 30 seconds, which allows users to request a response from another user
+const cooldownStage = () => {
+
+}
+
+const getSet = () => {
   // If we don't have a current set, set it to the first one
-  if (gameState.currentSet === null) gameState.currentSet = set1;
+  if (gameState.currentSet === null) return gameState.currentSet = set1;
   // If we do have a current set, advance it to the next one
-  else if (gameState.currentSet === set1) gameState.currentSet = set2;
-  else if (gameState.currentSet === set2) gameState.currentSet = set3;
+  else if (gameState.currentSet === set1) return gameState.currentSet = set2;
+  else if (gameState.currentSet === set2) return gameState.currentSet = set3;
   // If we've reached the end of the game publish the closing message and end the game
-  else {
-    // Publish the closing message
-    io.emit("end-message", closingMessage);
-    // Reset the gamestate
-    gameState = initialGameState;
-    // Set the Postgame to true
-    inPostgameCooldown = true;
-  }
+  else return 'game over'
 }
 
-const getCurrentPrompt = (index) => {
+const getPrompt = (index) => {
   // If we don't have a current prompt, set it to the first one in the current set
-  if (gameState.currentPrompt === null) gameState.currentPrompt = gameState.currentSet[index];
+  if (gameState.currentPrompt === null) return gameState.currentSet[0];
   // If we do have a current prompt, and we haven't reached the end of the set, set it to the next one in the set
   else if (index < gameState.currentSet.length) gameState.currentPrompt = gameState.currentSet[index];
   // If we have reached the end of the set, advance the set and set the current prompt to the first one in the new set
   else if (index >= gameState.currentSet.length) {
-    getPromptSet();
+    getSet();
     gameState.currentPrompt = gameState.currentSet[0];
   }
 }
-
-
-
-// Once the game starts. publish the locations of all the users, and publish the first prompt, grabbing the first array of the selected set, starting with the first. This is the start of the game loop.
-// After 30 seconds, start the countdown timer
-// Once the timer has run out, restart the countdown as a cooldown timer, allowing users a chance to use the reponse-request socket
-// Once the cooldown timer has finished, start the countdown again and grab the next array of the selected set
-// Repeat the game loop, moving on to new array until the set runs out, then switching to the next set, until that one runs out. Once we've finished the third set, publish the ending message.
-// Once the ending message has been published. Leave everything as is for 5 minutes, then setting the gamestate started back to false, marking the end of the game and setting up to begin a new one.
-
 // ------------------------------------------
 // Start the server
 httpServer.listen(3000);
